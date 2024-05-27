9a5c17d0a9cd1eb2cba2fcaf06b4a98959656387
sanghyunna <106161726+sanghyunna@users.noreply.github.com>
2024-05-28 02:45:44 +0900

    implementation

--- main.py
+++ main.py
@@ -22,25 +22,24 @@ goto_file_dir = "./goto.csv"
 
 #input file 입력받는 걸로 바꿀 예정
 token_list = []
-if len(sys.argv) > 1:
-    for arg in sys.argv:
-        token_list.append(arg)
-    del(token_list[0])
-else:
+if len(sys.argv) == 2:
+    input_file_dir = sys.argv[1]
+    token_list = get_tokens(input_file_dir)
+elif len(sys.argv) == 1:
     print("Please enter target string to parse.")
+    sys.exit()
+else:
+    print("Invalid input.")
     sys.exit()
 
 ## Read action, goto table
-action_df = read_csv_file(action_file_dir)
-goto_df = read_csv_file(goto_file_dir)
-
+action_df = read_csv_file(action_file_dir).astype(str)
+goto_df = read_csv_file(goto_file_dir).astype(str)
 
 #test파일
-print(action_df.head())
-print(goto_df.head())
+# print(action_df.head())
+# print(goto_df.head())
 
 syntax_analyzer(token_list, action_df, goto_df)
 
 print("Analysis complete.")
-
-input('')
